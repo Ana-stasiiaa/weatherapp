6a5aea4ef91f26a77a4e25d99dc39e51372dc608
Ana-stasiiaa <116268105+Ana-stasiiaa@users.noreply.github.com>
2022-10-26 13:46:20 +0300

    updated css for units

--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ iconElement.setAttribute(
   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  
     celsiusTemperature = response.data.main.temp;
 
 }
